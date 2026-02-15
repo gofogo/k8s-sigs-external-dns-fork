cf30d7b93ba6c1a1599eba2b1a72b1c5e5a3812c
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2026-02-15 10:17:24 +0000

    refactore(gen): shared functions. file writes
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

--- internal/gen/docs/utils/utils.go
+++ internal/gen/docs/utils/utils.go
@@ -27,6 +27,7 @@ import (
 	"golang.org/x/text/language"
 )
 
+// WriteToFile writes the given content to a file, creating or truncating it.
 func WriteToFile(filename string, content string) error {
 	return os.WriteFile(filename, []byte(content), 0644)
 }
